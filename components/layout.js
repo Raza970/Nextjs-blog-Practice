30a1f76cd41c50856dda208bdd1b2aef673ff65c
unknown <raza39701@gmail.com>
2023-03-05 02:36:43 +0500

    first commit

--- components/layout.js
+++ components/layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children, home }) {
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
-        />
+        /> 
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import utilStyles from "../styles/utils.module.css";
-import Layout, { siteTitle } from '../components/layout';
+import Layout, { siteTitle } from "../components/layout";
+import Image from "next/image";
+import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
-import Image from 'next/image';
+import Date from "../components/date";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
 const YourComponent = () => (
   <Image
@@ -15,25 +26,40 @@ const YourComponent = () => (
   />
 );
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-    <Head>
-      <title>{siteTitle}</title>
-    </Head>
-    <section className={utilStyles.headingMd}>
-      <p>[Helllo,I'm Raza .I'm a student of web3.You can contect me on <Link href=" https://twitter.com/RazaMalik99?t=f46Nh2OZDF6CHJ3H3CDEEw&s=08">Twitter</Link>]</p>
-      <p>
-        (This is a sample website - you’ll be building a site like this on{' '}
-        <Link href="https://nextjs.org/learn">our Next.js tutorial</Link>.)
-      </p>
-    </section>
-    {/* GOING TO NEXT PAGE  */}
-<div>
-<Link href="/post/first-post">NEXT POST</Link>
-</div>
-  </Layout>
-  
-);
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          [Helllo,I'm Raza .I'm a student of web3.You can contect me on{" "}
+          <Link href=" https://twitter.com/RazaMalik99?t=f46Nh2OZDF6CHJ3H3CDEEw&s=08">
+            Twitter
+          </Link>
+          ]
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <Link href="https://nextjs.org/learn">our Next.js tutorial</Link>.)
+        </p>
+      </section>
 
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
 }
