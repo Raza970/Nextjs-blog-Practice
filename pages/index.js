8e10293209c021fbbcfde93576d126ff68010fd8
unknown <raza39701@gmail.com>
2023-03-05 03:47:30 +0500

    third

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+              <Link href={`/post/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
